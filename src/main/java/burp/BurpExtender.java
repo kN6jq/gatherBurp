3f5e11db5129aa6f00c7b71459467c150213762a
Night <guest_xm17@163.com>
2023-08-08 22:56:36 +0800

    新增base64提交复杂数据

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Objects;
 
@@ -18,7 +19,7 @@ import static burp.dao.ConfigDAO.getToolConfig;
 import static burp.utils.Utils.*;
 
 
-public class BurpExtender  implements IBurpExtender, IContextMenuFactory{
+public class BurpExtender  implements IBurpExtender, IContextMenuFactory,IHttpListener{
 
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks iBurpExtenderCallbacks) {
@@ -29,6 +30,7 @@ public class BurpExtender  implements IBurpExtender, IContextMenuFactory{
         Utils.stderr = new PrintWriter(iBurpExtenderCallbacks.getStderr(), true);
         Utils.callbacks.setExtensionName(Utils.name);
         Utils.callbacks.registerContextMenuFactory(this);
+        Utils.callbacks.registerHttpListener(this);
         Utils.callbacks.addSuiteTab(mainUi);
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -95,10 +97,53 @@ public class BurpExtender  implements IBurpExtender, IContextMenuFactory{
         listMenuItems.add(new SqlMenu(responses));
         listMenuItems.add(new Log4jCheckMenu(responses));
         listMenuItems.add(new DropHostMenu(responses));
+        listMenuItems.add(new Base64DataMenu(responses));
         return listMenuItems;
     }
 
 
-
-
+    @Override
+    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
+//        if (toolFlag == IBurpExtenderCallbacks.TOOL_REPEATER && messageIsRequest){
+//            IHttpRequestResponse[] baseRequestResponse = new IHttpRequestResponse[]{messageInfo};
+//            IHttpRequestResponse iHttpRequestResponse = baseRequestResponse[0];
+//            IRequestInfo iRequestInfo = helpers.analyzeRequest(baseRequestResponse[0]);
+//            String request = Utils.helpers.bytesToString(iHttpRequestResponse.getRequest());
+//            String method = iRequestInfo.getMethod();
+//            String body = new String(messageInfo.getRequest());
+//            if (method.equals("POST")){
+//                // 获取当前时间
+//                long time = System.currentTimeMillis();
+//                if (body.contains("<base64data>") && body.contains("</base64data>")){
+//                    String data = body.substring(body.indexOf("<base64data>")+12,body.indexOf("</base64data>"));
+//                    byte[] decodedData = Base64.getDecoder().decode(data);
+//                    // 创建一个新的byte数组来存储替换后的数据
+//                    byte[] newBytes = new byte[request.length() - data.length() + decodedData.length];
+//                    // 将request的前部分拷贝到新数组中
+//                    System.arraycopy(request.getBytes(), 0, newBytes, 0, body.indexOf("<base64data>"));
+//                    // 将解码后的数据拷贝到新数组中
+//                    System.arraycopy(decodedData, 0, newBytes, body.indexOf("<base64data>"), decodedData.length);
+//                    // 将request的后部分拷贝到新数组中
+//                    System.arraycopy(request.getBytes(), body.indexOf("</base64data>") + 13, newBytes, body.indexOf("<base64data>") + decodedData.length, request.length() - body.indexOf("</base64data>") - 13);
+//                    Utils.stdout.println(System.currentTimeMillis()-time);
+//                    Utils.callbacks.makeHttpRequest(iHttpRequestResponse.getHttpService(),newBytes );
+//                }
+//            }
+//
+//        }
+        if (toolFlag == IBurpExtenderCallbacks.TOOL_REPEATER && messageIsRequest){
+            byte[] request = messageInfo.getRequest();
+            String requestStr = helpers.bytesToString(request);
+            Utils.stdout.println(requestStr);
+            if (requestStr.contains("<datab64>")){
+                String data = requestStr.substring(requestStr.indexOf("<datab64>")+9,requestStr.indexOf("</datab64>"));
+                byte[] decodedData = Base64.getDecoder().decode(data);
+                byte[] newBytes = new byte[request.length - data.length() + decodedData.length];
+                System.arraycopy(request, 0, newBytes, 0, requestStr.indexOf("<datab64>"));
+                System.arraycopy(decodedData, 0, newBytes, requestStr.indexOf("<datab64>"), decodedData.length);
+                System.arraycopy(request, requestStr.indexOf("</datab64>") + 10, newBytes, requestStr.indexOf("<datab64>") + decodedData.length, request.length - requestStr.indexOf("</datab64>") - 10);
+                messageInfo.setRequest(newBytes);
+            }
+        }
+    }
 }
