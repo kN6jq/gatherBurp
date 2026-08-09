a4582054c4c582a5319c769e85014a2435e53124
Xm17 <27048404+kN6jq@users.noreply.github.com>
2026-08-09 18:43:59 +0800

    refactor: NucleiMenu简化为lambda表达式
    - 移除冗余的ActionListener/Runnable匿名类
    - 使用lambda表达式和AbstractScanMenu的线程模板
    - 添加异常捕获防止线程静默失败

--- src/main/java/burp/menu/NucleiMenu.java
+++ src/main/java/burp/menu/NucleiMenu.java
@@ -2,24 +2,19 @@ package burp.menu;
 
 import burp.IHttpRequestResponse;
 import burp.utils.Nuclei;
+import burp.utils.Utils;
 
 import javax.swing.*;
-import java.awt.event.ActionListener;
 
 public class NucleiMenu extends JMenuItem {
     public NucleiMenu(IHttpRequestResponse[] requestResponses) {
-        this.setText("^_^ Nuclei Template");
-        this.addActionListener(new ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Nuclei.Generate(requestResponses);
-                    }
-                });
-                thread.start();
-
+        super("^_^ Nuclei Template");
+        addActionListener(e -> new Thread(() -> {
+            try {
+                Nuclei.Generate(requestResponses);
+            } catch (Exception ex) {
+                Utils.stderr.println("Nuclei template generation error: " + ex.getMessage());
             }
-        });
+        }).start());
     }
 }
